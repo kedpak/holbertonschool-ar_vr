7d9ae12d21baa87fdc4e4f8806cec348d29cd188
Kevin Pak <kedpak@yahoo.com>
2018-05-14 22:36:14 -0700

    Add tag

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine(string.Format("Percent: {0:0.00%}", percent));
-		Console.WriteLine(string.Format("${0:n2}", currency));
+		Console.WriteLine(string.Format("Currency: ${0:n2}", currency));
 	}
 }
