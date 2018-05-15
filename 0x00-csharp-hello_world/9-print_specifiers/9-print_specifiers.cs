9ca8c27172f10af313b76c4763a003641b27f57d
Kevin Pak <kedpak@yahoo.com>
2018-05-14 22:34:38 -0700

    first commit

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine(string.Format("Percent: {0:0.00%}", percent));
+		Console.WriteLine(string.Format("${0:n2}", currency));
+	}
+}
